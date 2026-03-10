f570e638b814d21eac8188d1d4e67e39f6d0c26c
Elohá <elohavergararo@gmail.com>
2026-03-10 02:24:42 +0100

    Install Tailwind and add configuration files manually

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Ajusta la ruta según tu proyecto
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
